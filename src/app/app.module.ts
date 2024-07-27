efe077a25cc77bbb6118aa226be42d90eb5c81d0
Nishant Dev <nishantdev@Nishants-MacBook-Air.local>
2024-07-27 14:55:47 +0530

    added-nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
+import { MatIconModule } from '@angular/material/icon';
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +17,12 @@ import { PipelineDashComponent } from './pages/pipeline-dash/pipeline-dash.compo
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,16 +34,21 @@ import { SideNavComponent } from './components/side-nav/side-nav.component';
     PipelineDashComponent,
     HeaderComponent,
     FooterComponent,
-    SideNavComponent
+
+    SideNavComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatIconModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatListModule,
   ],
-  providers: [
-    provideClientHydration(),
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideClientHydration(), provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
